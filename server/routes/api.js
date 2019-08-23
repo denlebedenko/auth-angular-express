2eadfed06ee73832e01605c971ad4167f1d5d7a7
denlebedenko <lebedenkodd@gmail.com>
2019-08-23 12:08:34 +0300

    generate jwt token

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
+
 const User = require('../models/user');
 const Book = require ('../models/book');
 
@@ -25,7 +27,9 @@ router.post('/register', (req, res) => {
         if(error){
             console.log(error)
         } else {
-            res.status(200).send(registeredUser)
+            let payload = {subject: registeredUser._id};
+            let token = jwt.sign(payload, 'secret_key');
+            res.status(200).send({token})
         }
     })
 })
@@ -42,8 +46,10 @@ router.post('/login', (req, res) => {
                 res.status(401).send('invalid email');
             } else if (user.password !== userData.password) {
                 res.status(401).send('invalid password');
-            } else { 
-                res.status(200).send(user)
+            } else {
+                let payload = {subject: user._id}
+                let token = jwt.sign(payload, 'secret_key') 
+                res.status(200).send({token})
             }
         }
     })
